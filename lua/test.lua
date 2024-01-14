708373b58bb0d62aa1c4f41847e2e0841be85beb
jiuliyemingzhi <r1907715047@outlook.com>
2024-01-15 00:09:37 +0800

    init

--- lua/test.lua
+++ lua/test.lua
@@ -1,4 +1,9 @@
 abc = 1;
-_abc = 2 + 0x12;
+_abc = 2 + 0
+xi12 = 2e+90;
+xx = "xxx\"" .. 'yyy\''
 --ok = 4; \n _abc = 4;
-print(_abc, 00000012, 12)
+print(_abc, 00000012, 12, xi12, xx)
+function abc()
+
+end
--- src/parser/token.rs
+++ src/parser/token.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::fmt::Debug;
 use std::fs::File;
 use std::io::Read;
@@ -23,10 +24,13 @@ impl Default for TokenExtras {
 #[logos(extras = TokenExtras)]
 #[logos(skip r"[ \t]")]
 pub enum TokenEnum {
-    #[regex(r"[\n\f]", line)]
+    #[regex(r"(\r\n)|[\n\f\r]", line)]
     Line,
-    #[regex(r"--[^\n\f]*", to_string_token)]
+    #[regex(r"--[^\n\f\r]*", str_lexer)]
     Comment(Token<String>),
+    #[regex(r#""([^"\\]|\\.|"")*""#, string_lexer)]
+    #[regex(r#"'([^'\\]|\\.|'')*'"#, string_lexer)]
+    QuotedString(Token<String>),
     #[token("=")]
     Equal,
     #[token("+")]
@@ -35,14 +39,24 @@ pub enum TokenEnum {
     Semicolon,
     #[token(",")]
     Comma,
+    #[token("..")]
+    DoubleDot,
     #[token("(")]
     ParenthesesLeft,
     #[token(")")]
     ParenthesesRight,
-    #[regex(r"[_a-zA-Z][_0-9a-zA-Z]*", to_string_token)]
+    #[regex(r"[_a-zA-Z][_0-9a-zA-Z]*", str_lexer)]
     Name(Token<String>),
-    #[regex("(0x)?[0-9]+", to_u64_token)]
+    #[regex("(0x)?[0-9]+", u64_lexer)]
     Int(Token<u64>),
+    #[regex(r"([0-9]*\.[0-9]+([eE][+-]?[0-9]+)?)|([0-9]+\.[0-9]*([eE][+-]?[0-9]+)?)|([0-9]+[eE][+-]?[0-9]+)", f64_lexer)]
+    Float(Token<f64>),
+    #[token("function")]
+    Function,
+    #[token("end")]
+    End,
+    #[token("local")]
+    Local,
 }
 
 #[derive(Debug)]
@@ -51,6 +65,7 @@ pub struct Token<T> {
     pub span: Span,
     pub v: T,
 }
+
 
 impl<T> Token<T> {
     #[inline]
@@ -63,33 +78,34 @@ impl<T> Token<T> {
     }
 }
 
-impl Token<String> {
-    #[inline]
-    fn from_lexer(lex: &mut Lexer<TokenEnum>) -> Option<Token<String>> {
-        Some(Self::new(lex.slice().to_string(), lex.span(), lex.extras.line_breaks))
+#[inline]
+fn str_lexer(lex: &mut Lexer<TokenEnum>) -> Option<Token<String>> {
+    Some(Token::new(lex.slice().to_string(), lex.span(), lex.extras.line_breaks))
+}
+
+#[inline]
+fn string_lexer(lex: &mut Lexer<TokenEnum>) -> Option<Token<String>> {
+    let span = lex.span();
+    let x = lex.slice();
+    Some(Token::new(x[..x.len() - 1].replace("\\", ""), span, lex.extras.line_breaks))
+}
+
+fn u64_lexer(lex: &mut Lexer<TokenEnum>) -> Option<Token<u64>> {
+    let mut x = lex.slice();
+    let radix = if x.starts_with("0x") {
+        x = &x[2..];
+        16
+    } else { 10 };
+    match u64::from_str_radix(x, radix) {
+        Ok(v) => Some(Token::new(v, lex.span(), lex.extras.line_breaks)),
+        Err(err) => print_err(lex, Box::new(err))
     }
 }
 
-impl Token<u64> {
-    #[inline]
-    fn from_lexer(lex: &mut Lexer<TokenEnum>) -> Option<Token<u64>> {
-        let mut x = lex.slice();
-        let radix = if x.starts_with("0x") {
-            x = &x[..2];
-            16
-        } else { 10 };
-        match u64::from_str_radix(x, radix) {
-            Ok(v) => Some(Token::new(v, lex.span(), lex.extras.line_breaks)),
-            Err(err) => {
-                let source = &lex.source()[..lex.extras.line_start];
-                println!("{:?}: {}, {}\nerr: {:?}",
-                         lex.extras.file_path,
-                         lex.extras.line_breaks,
-                         &source[..source.as_bytes().iter().position(|&c| c == b'\n').unwrap_or(source.len())],
-                         err);
-                None
-            }
-        }
+fn f64_lexer(lex: &mut Lexer<TokenEnum>) -> Option<Token<f64>> {
+    match lex.slice().parse() {
+        Ok(v) => Some(Token::new(v, lex.span(), lex.extras.line_breaks)),
+        Err(err) => print_err(lex, Box::new(err))
     }
 }
 
@@ -106,20 +122,22 @@ impl TokenEnum {
     }
 }
 
+#[inline]
+fn print_err<T>(lex: &Lexer<TokenEnum>, err: Box<dyn Error>) -> Option<Token<T>> {
+    let source = &lex.source()[lex.extras.line_start..];
+    println!("{:?}: {}, {} {}\nerr: {:?}",
+             lex.extras.file_path,
+             lex.extras.line_breaks,
+             lex.slice(),
+             &source[..source.as_bytes().iter().position(|&c| c == b'\n' || c == b'\r').unwrap_or(source.len())],
+             err);
+    None
+}
 
 #[inline]
 fn line(lex: &mut Lexer<TokenEnum>) -> logos::Skip {
     lex.extras.line_breaks += 1;
-    lex.extras.line_start = lex.span().end;
+    lex.extras.line_start = lex.span().start;
     logos::Skip
 }
 
-#[inline]
-fn to_string_token(lex: &mut Lexer<TokenEnum>) -> Option<Token<String>> {
-    Token::<String>::from_lexer(lex)
-}
-
-#[inline]
-fn to_u64_token(lex: &mut Lexer<TokenEnum>) -> Option<Token<u64>> {
-    Token::<u64>::from_lexer(lex)
-}
